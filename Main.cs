0170b1fda14ba38978ccab74008c28b2cd343b43
Srg90 <usikov90@mail.ru>
2022-02-19 19:51:37 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,58 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPI3._1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            IList<Reference> selectedElementRefList = uidoc.Selection.PickObjects(ObjectType.Face, "Выберете элементы стен");
+            var wallList = new List<Wall>();
+            var volume = new List<double>();
+            double sum = 0;
+            string Vol = string.Empty;
+
+            foreach (var selectedElement in selectedElementRefList)
+            {
+                //Wall oWall = doc.GetElement(selectedElement) as Wall;
+                //wallList.Add(oWall);
+                Element element = doc.GetElement(selectedElement);
+                if (element is Wall)
+                {
+                    Wall oWall = (Wall)element;
+                    wallList.Add(oWall);
+                }
+            }
+            foreach (var wall in wallList)
+            {
+                Parameter wallVolume = wall.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED);
+                double V = UnitUtils.ConvertFromInternalUnits(wallVolume.AsDouble(), UnitTypeId.CubicMeters);
+                volume.Add(V);
+            }
+            foreach (var i in volume)
+            {
+                sum += i;
+            }
+            string SumVolume = sum.ToString();
+            Vol += $"Объем: {SumVolume}м3{Environment.NewLine}Общее количество элементов стен: {wallList.Count}";
+
+            TaskDialog.Show("Selection", Vol);
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ WallFilter.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPI3._1
+{
+    public class WallFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            return elem is Wall;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+}
+
